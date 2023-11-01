1bd78171a3b86b6f2351c052fca9025744523c46
Tobias la Fleur <tobias.lafleur98@gmail.com>
2023-11-01 17:57:53 +0100

    feat: added rules

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,3 +1,28 @@
+const rules = [
+  'Your lives will be linked. If your partner dies, you die.',
+  'Trading is only allowed within the team.',
+  'No AH or sending items/gold/consumeables from main.',
+  'No asking for help.',
+  'No grouping up with others.',
+  'Addons like RestedXP are not allowed.',
+  'Bonus objectives can only be completed once.',
+  'If you or your partner dies, you both have to down your drinks before going again.',
+  'Every time you level up, you drink.',
+  'if you run away from a fight, drink.',
+  'Whenever someone in the guild dies (teams count as one), drink.',
+];
+
 export default function Page() {
-  return <main className="flex flex-1 items-center justify-center"></main>;
+  return (
+    <main className="flex flex-1 flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-semibold text-gray-200">Rules</h1>
+      <ul className="flex list-decimal flex-col gap-2 text-gray-200">
+        {rules.map(rule => (
+          <li key={rule}>
+            <p>{rule}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
